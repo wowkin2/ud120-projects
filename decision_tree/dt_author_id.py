8d7c1e86dd37636350a7bc96239836267d65aa8c
Volodymyr <volodymyr.spodaryk@gmail.com>
2016-09-02 17:27:03 +0300

    Second and Third lessons complete

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -26,6 +26,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc_min_samples_split = accuracy_score(pred, labels_test)
+
+print({
+    'accuracy': acc_min_samples_split,
+    'number of features': len(features_train[0])
+})
 #########################################################
 
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,30 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+# clf = svm.SVC(C=1.0, kernel='linear', gamma='auto')
+clf = svm.SVC(C=10000, kernel='rbf', gamma='auto')
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
+
+t0 = time()
+pred = clf.predict(features_test)
+print("predition time:", round(time()-t0, 3), "s")
+
+accuracy = accuracy_score(pred, labels_test)
+print(pred, accuracy)
+print(pred[100])
+print(pred[10], pred[26], pred[50])
+print(len(pred), len(filter(lambda x: x == 1, pred)), len(filter(lambda x: x == 0, pred)))
 
 #########################################################
-
-
